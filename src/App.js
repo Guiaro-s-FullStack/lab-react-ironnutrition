35956a81c37d4d3cf507721db6064117a5750807
Arthur <arthurguiaro@gmail.com>
2023-11-07 19:53:34 -0300

    Iteration 1 | Render a Simple List(Arthur)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, Card } from "antd"
+import foodsData from "./foods.json"
+import { useState } from "react"
 
 function App() {
+  const [foods, setFoods] = useState(foodsData)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {foods.map(food => {
+        return (<div>
+          <p>{food.name}</p>
+          <img  src={food.image} width={140}/>
+        </div>
+        )
+      })}
     </div>
   );
 }
