00ca5c122985fabe6f0ce8a10b3ec34a5258205c
Fiote <brenoft14@gmail.com>
2023-11-07 20:15:25 -0300

    Iteration 3 | Render a List of FoodBox Components (Breno)

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import './App.css';
 import { Button, Card } from "antd"
 import foodsData from "./foods.json"
 import { useState } from "react"
+import FoodBox from './components/FoodBox';
+
 
 function App() {
   const [foods, setFoods] = useState(foodsData)
   return (
     <div className="App">
       {foods.map(food => {
-        return (<div>
-          <p>{food.name}</p>
-          <img  src={food.image} width={140}/>
-        </div>
-        )
+        return <FoodBox food={{
+          name: food.name,
+          calories: food.calories,
+          image: food.image,
+          servings: food.servings,
+                }}/>
       })}
     </div>
   );
